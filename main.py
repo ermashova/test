a981ebc8d1151e93771bddc3d65f7c93460a6680
Marina <ermashova@list.ru>
2023-04-26 09:20:39 +0300

    конкатенация списков, редактирование требований

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-with open('table.csv', 'w', encoding='utf-8-sig', newline='') as file:
-    writer = csv.writer(file, delimiter=';')
-    writer.writerow([
-        'Наименование', 'Цена', 'Локация', 'Описание'])
+# Создаем пустые списки для каждой колонки
+names = []
+prices = []
+locations = []
+descriptions = []
+
+# Читаем страницу и парсим данные
 url = 'https://businessesforsale.ru/krasnodarskiy-kray-kupit-gostinicu/'
 response = requests.get(url=url)
 response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text, 'lxml')
-name = [x.text.strip() for x in soup.find_all('a', class_='title visible-desktop visible-tablet')]
-price = [x.text for x in soup.find_all('span', class_='price')]
-location = [x.text for x in soup.find_all('a', class_='info_location')]
-description = [x.text for x in soup.find_all('p', class_='desc')]
-for name, price, location, description in zip(name, price, location, description):
-    file = open('table.csv', 'a', encoding='utf-8-sig', newline='')
+
+# Добавляем данные в списки
+names += [x.text.strip() for x in soup.find_all('a', class_='title visible-desktop visible-tablet')]
+prices += [x.text for x in soup.find_all('span', class_='price')]
+locations += [x.text for x in soup.find_all('a', class_='info_location')]
+descriptions += [x.text for x in soup.find_all('p', class_='desc')]
+
+# Записываем данные в CSV-файл
+with open('table.csv', 'w', encoding='utf-8-sig', newline='') as file:
     writer = csv.writer(file, delimiter=';')
     writer.writerow([
-        name, price, location, description])
-file.close()
+        'Наименование', 'Цена', 'Локация', 'Описание'])
+    for _ in zip(names, prices, locations, descriptions):
+        writer.writerow(_)
+
 print('Файл table.csv создан')
